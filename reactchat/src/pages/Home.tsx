154794fedba2f7f6c6b6bc01906a357afc4fd882
ramyo564 <yohan032yohan@gmail.com>
2023-07-21 14:39:14 +0900

    Update Home.tsx

--- reactchat/src/pages/Home.tsx
+++ reactchat/src/pages/Home.tsx
@@ -1,10 +1,11 @@
 import { Box, CssBaseline } from "@mui/material";
+import PrimaryAppBar from "./templates/PrimaryAppBar";
 
 const Home = () => {
     return(
         <Box sx={{ display: "flex" }}>
             <CssBaseline />
-            home
+            <PrimaryAppBar />
         </Box>
     );
 };
