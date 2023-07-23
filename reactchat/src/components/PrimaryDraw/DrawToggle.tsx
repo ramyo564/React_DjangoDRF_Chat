7ae5d973f8bf0c7f0b1d759d027ab951022d9c8b
ramyo564 <yohan032yohan@gmail.com>
2023-07-23 12:47:52 +0900

    Build : Primary Draw (Part-2) Functionality

--- reactchat/src/components/PrimaryDraw/DrawToggle.tsx
+++ reactchat/src/components/PrimaryDraw/DrawToggle.tsx
@@ -1,22 +1,32 @@
-import ChevronLeft from "@mui/icons-material/ChevronLeft";
+import { ChevronRight, ChevronLeft } from "@mui/icons-material";
 import { Box, IconButton } from "@mui/material";
+import React from "react";
 
+type Props = {
+    open: boolean;
+    handleDrawerOpen: () => void;
+    handleDrawerClose: () => void;
+};
 
-const DrawerToggle = () => {
-    return(
-        <Box 
-            sx={{
-                height:"50px",
-                display: "flex",
-                alignItems: "content",
-                justifyContent: "center",
-            }}
-        >
-            <IconButton>
-                <ChevronLeft/>
-            </IconButton>
-        </Box>
-    )
+const DrawerToggle: React.FC<Props> = ({
+    open,
+    handleDrawerClose,
+    handleDrawerOpen,
+    }) => {
+        return(
+            <Box 
+                sx={{
+                    height:"50px",
+                    display: "flex",
+                    alignItems: "content",
+                    justifyContent: "center",
+                }}
+            >
+                <IconButton onClick={open ? handleDrawerClose : handleDrawerOpen}>
+                    {open ? <ChevronLeft/> : <ChevronRight/>}
+                </IconButton>
+            </Box>
+        )
 }
 
 export default DrawerToggle;
--- reactchat/src/pages/templates/PrimaryDraw.tsx
+++ reactchat/src/pages/templates/PrimaryDraw.tsx
@@ -1,13 +1,48 @@
-import { Drawer, Box, useMediaQuery, Typography, useTheme } from "@mui/material";
+import { Box, useMediaQuery, Typography, useTheme, styled } from "@mui/material";
 import { useState, useEffect } from "react";
 import DrawerToggle from "../../components/PrimaryDraw/DrawToggle";
+import MuiDrawer from "@mui/material/Drawer";
 
 const PrimaryDraw = () => {
     const theme = useTheme();
     const below600 = useMediaQuery("(max-width:599px)");
     const [open, setOpen] = useState(!below600);
 
-    console.log(below600)
+    // console.log(below600)
+
+    const openedMixin = () => ({
+        transition: theme.transitions.create("width", {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.enteringScreen,
+        }),
+        overflowX: "hidden",
+    });
+
+    const closedMixin = () => ({
+        transition: theme.transitions.create("width", {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.enteringScreen,
+        }),
+        overflowX: "hidden",
+        width: theme.primaryDraw.closed,
+    });
+
+    const Drawer = styled(
+        MuiDrawer,
+        {}
+    )(({theme, open}) => ({
+        width: theme.primaryDraw.width,
+        whiteSpace: "nowrap",
+        boxSizing: "border-box",
+        ...(open && {
+            ...openedMixin(),
+            "& .MuiDrawer-paper": openedMixin(),
+        }),
+        ...(!open && {
+            ...openedMixin(),
+            "& .MuiDrawer-paper": closedMixin(),
+        }),
+    }));
 
     useEffect(() => {
         setOpen(!below600);
@@ -43,7 +78,11 @@ const PrimaryDraw = () => {
                     width: open ? "auto" : "100%",
                 }}
             >
-                <DrawerToggle/>
+                <DrawerToggle 
+                    open={open}
+                    handleDrawerClose={handleDrawerClose}
+                    handleDrawerOpen={handleDrawerOpen}
+                />
                 {[...Array(50)].map((_, i)=>(
                 <Typography key={i} paragraph>
                     {i +1}
