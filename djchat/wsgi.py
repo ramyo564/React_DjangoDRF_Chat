2006529f12abb545768ee4ccc2981066122e2bd0
ramyo564 <yohan032yohan@gmail.com>
2023-07-28 10:30:52 +0900

    Build : Implimenting WebSockets - Routing

--- djchat/wsgi.py
+++ djchat/wsgi.py
@@ -9,8 +9,21 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 
 import os
 
-from django.core.wsgi import get_wsgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+
+# from django.core.wsgi import get_wsgi_application
+from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djchat.settings')
+django_application = get_asgi_application()
 
-application = get_wsgi_application()
+from . import urls # noqa isort: skip
+
+
+# application = get_wsgi_application()
+application = ProtocolTypeRouter(
+    {
+        "http": get_asgi_application(),
+        "websocket": URLRouter(urls.websocket_urlpatterns),
+    }
+)
