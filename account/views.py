ebc9ef618d9dec59241f1e89789ed962d70b8483
ramyo564 <yohan032yohan@gmail.com>
2023-08-04 17:49:30 +0900

    Build: Protecting API Endpoints

--- account/views.py
+++ account/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets
+from rest_framework.response import Response
+from drf_spectacular.utils import extend_schema
+
+from .models import Account
+from .serializers import AccountSerializer
+from .schemas import user_list_docs
+from rest_framework.permissions import IsAuthenticated
+
+class AccountViewSet(viewsets.ViewSet):
+    queryset = Account.objects.all()
+    permission_classes = [IsAuthenticated]
+    
+    @user_list_docs
+    def list(self, request):
+        user_id = request.query_params.get("user_id")
+        queryset = Account.objects.get(id=user_id)
+        serializer = AccountSerializer(queryset)
+        return Response(serializer.data)
--- reactchat/src/services/AuthServices.ts
+++ reactchat/src/services/AuthServices.ts
@@ -3,6 +3,36 @@ import { AuthServiceProps } from "../@types/auth-service";
 
 
 export function useAuthService(): AuthServiceProps {
+
+    const getUserDetails = async () => {
+        try {
+            const userId = localStorage.getItem("userId")
+            const accessToken = localStorage.getItem("access_token")
+            const response = await axios.get(
+                `http://127.0.0.1:8000/api/account/?user_id=${userId}`,{
+                    headers:{
+                        Authorization: `Bearer ${accessToken}`
+                    }
+                }
+                
+            );
+            const userDetails = response.data
+            localStorage.setItem( "username", userDetails.username );
+
+        } catch (err: any) {
+            return err;
+        }
+    }
+    const getUserIdFromToken = (access : string) => {
+        const token = access
+        const tokenParts = token.split('.')
+        const encodedPayLoad = tokenParts[1]
+        const decodedPayLoad = atob(encodedPayLoad)
+        const payLoadData = JSON.parse(decodedPayLoad)
+        const userId = payLoadData.user_id
+
+        return userId
+    }
     const login = async (username: string, password: string) => {
         try {
             const response = await axios.post(
@@ -11,7 +41,15 @@ export function useAuthService(): AuthServiceProps {
                     password,
                 }
             );
-            console.log(response)
+
+            const { access, refresh } = response.data;
+
+            // Save the tokens to local storage
+            localStorage.setItem( "access_token", access );
+            localStorage.setItem( "refresh_token", refresh );
+            localStorage.setItem( "userId", getUserIdFromToken(access))
+
+            getUserDetails()
         } catch (err: any) {
             return err;
         }
