a598bd047f8b695f1bdfc2cd629645f93a0435a4
ramyo564 <yohan032yohan@gmail.com>
2023-08-08 18:53:17 +0900

    Refactoring: useChatServices Custom Hook

--- reactchat/src/components/Main/MessageInterface.tsx
+++ reactchat/src/components/Main/MessageInterface.tsx
@@ -1,9 +1,4 @@
-import { useState } from "react";
-import { useParams } from "react-router-dom";
-import useWebSocket from "react-use-websocket";
-import useCrud from "../../hooks/useCrud";
-import { Server } from "../../@types/server.d";
-import { useAuthService } from "../../services/AuthServices";
+
 import {
   Avatar,
   Box,
@@ -17,7 +12,9 @@ import {
 } from "@mui/material";
 import MessageInterfaceChannels from "./MessageInterfaceChannels";
 import Scroll from "./Scroll";
-import React from "react";
+import { useParams } from "react-router-dom";
+import { Server } from "../../@types/server.d";
+import useChatWebSocket from "../../services/chatService";
 
 interface SendMessageData {
   type: string;
@@ -35,70 +32,21 @@ interface Message {
   timestamp: string;
 }
 
+
 const messageInterface = (props: ServerChannelProps) => {
   const { data } = props;
   const theme = useTheme();
-  const [newMessage, setNewMessage] = useState<Message[]>([]);
-  const [message, setMessage] = useState("");
+  
   const { serverId, channelId } = useParams();
-  const server_name = data?.[0]?.name ?? "Server";
-  const { logout, refreshAccessToken } = useAuthService();
-  const { fetchData } = useCrud<Server>(
-    [],
-    `/messages/?channel_id=${channelId}`
+
+  const { newMessage, message, setMessage, sendJsonMessage } = useChatWebSocket(
+    channelId || "",
+    serverId || ""
   );
 
-  const socketUrl = channelId
-    ? `ws://127.0.0.1:8000/${serverId}/${channelId}`
-    : null;
+  const server_name = data?.[0]?.name ?? "Server";
 
-  const [reconnectionAttempt, setReconnectionAttempt] = useState(0);
-  const maxConnectionAttempts = 4;
 
-  const { sendJsonMessage } = useWebSocket(socketUrl, {
-    onOpen: async () => {
-      try {
-        const data = await fetchData();
-        setNewMessage([]);
-        setNewMessage(Array.isArray(data) ? data : []);
-        console.log("Connected!!!");
-      } catch (error) {
-        console.log(error);
-      }
-    },
-    onClose: (event: CloseEvent) => {
-      if (event.code == 4001) {
-        console.log("Authentication Error");
-        refreshAccessToken().catch((error) => {
-          if(error.response && error.response.status === 401){
-            logout();
-          
-          }
-        });
-      }
-      console.log("Close");
-      setReconnectionAttempt((prevAttempt) => prevAttempt + 1);
-    },
-    onError: () => {
-      console.log("Error!");
-    },
-    onMessage: (msg) => {
-      const data = JSON.parse(msg.data);
-      setNewMessage((prev_msg) => [...prev_msg, data.new_message]);
-      setMessage("");
-    },
-    shouldReconnect: (closeEvent) => {
-      if (
-        closeEvent.code === 4001 && 
-        reconnectionAttempt >= maxConnectionAttempts
-        ) {
-          setReconnectionAttempt(0);
-          return false;
-      }
-      return true;
-    },
-    reconnectInterval: 1000,
-  });
 
   const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
     if (e.key === "Enter") {
